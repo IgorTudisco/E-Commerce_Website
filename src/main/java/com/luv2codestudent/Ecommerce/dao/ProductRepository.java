52ba4cbcd667a217779e7d0cba53bc12df26e074
Igor Tudisco <igortudisco@hotmail.com>
2022-05-26 21:33:57 -0300

    Implemented - Pageable by category

--- src/main/java/com/luv2codestudent/Ecommerce/dao/ProductRepository.java
+++ src/main/java/com/luv2codestudent/Ecommerce/dao/ProductRepository.java
@@ -1,9 +1,15 @@
 package com.luv2codestudent.Ecommerce.dao;
 
 import com.luv2codestudent.Ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin("http://localhost:4200") // passing the Angular address
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+
 }
